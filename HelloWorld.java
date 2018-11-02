6e0f16b2a5815d3ed651e5d085a47d0dd8bd9857
p70062 <44706104+p70062@users.noreply.github.com>
2018-11-02 12:31:14 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
  class HelloWorld {
  public static void main(String args[]){
-     System.out.println("Code build should be flagged as stable again.");
+     System.out.println("Class will be copied to Storage bucket");
  }
 }
