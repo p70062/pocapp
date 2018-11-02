6ce0178239b3f585123f0ac64bdd06e265155b25
p70062 <44706104+p70062@users.noreply.github.com>
2018-11-02 12:37:33 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
  class HelloWorld {
  public static void main(String args[]){
-     System.out.println("Class will be copied to Storage bucket");
+     System.out.println("Successful compilation");
  }
 }
