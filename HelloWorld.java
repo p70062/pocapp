901759d5267726ac9a4ee2eea026d4cceee618fc
p70062 <44706104+p70062@users.noreply.github.com>
2018-11-02 12:02:48 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
  class HelloWorld {
  public static void main(String args[]){
-     System.out.println("This should throw an error due to missing semicolon.")
+     System.out.println("Code build should be flagged as stable again.");
  }
 }
