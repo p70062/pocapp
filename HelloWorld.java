7ae521e691169d78509f4fa834caaa6159878bfb
p70062 <44706104+p70062@users.noreply.github.com>
2018-11-02 12:00:42 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
- class A {
+ class HelloWorld {
  public static void main(String args[]){
-     System.out.println("Hello World Version 4");
+     System.out.println("This should invoke compilation of the class.");
  }
 }
